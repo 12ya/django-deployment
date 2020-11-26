9747a0ea0141a79aa5a9cc8b85df1ed8b933ee55
12ya <68940374+12ya@users.noreply.github.com>
2020-11-26 19:13:32 +0900

    Update views.py

--- basic/basicapp/views.py
+++ basic/basicapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from basicapp.forms import UserProfileInfoForm, UserForm
 # Create your views here.
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
